9c67310fb5310c555564a61461d05a6469fec90e
“Hiba” <hibahachhouche11@gmail.com>
2023-06-15 19:02:29 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
- * description: C program with a put function
+ * Description: C program with a put function
  *
  * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
