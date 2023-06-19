33edee9504d694151f075a7907033b23e63b70a7
“Hiba” <hibahachhouche11@gmail.com>
2023-06-19 12:57:30 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Discription: 'print digit numbers of base 10'
+ *Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for(x = 0; x <= 9; x++)
+	{
+	printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
