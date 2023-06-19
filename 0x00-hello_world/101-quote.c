dbd3ca0ba84c61a067cd3d7abce6d07bc72ffe9b
“Hiba” <hibahachhouche11@gmail.com>
2023-06-19 16:21:11 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
 
 /**
-*main - Entry
+*main - Entry point
 *
-*Discription:  C program with a print a text function
+*Discription: 'C program that print text with a function'
 *
 *Return: 1 (NOT SUCCESS)
 */
 
 int main(void)
+
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
