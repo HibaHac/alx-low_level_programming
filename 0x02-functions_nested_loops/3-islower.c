8b68fab301034bc8fdaabdb27e9179f267449b46
“Hiba” <hibahachhouche11@gmail.com>
2023-06-20 16:47:54 -0700

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,16 @@
 
 /**
  * _islower - checks for lowercase character
- *@c: check character
+ * @c: the character to be checked
  * Return: 1 for lowercase charachter or 0 for anything else
  */
+
 int _islower(int c)
 
 {
 	if (c >= 97 && c <= 122)
 	{
-		return (1);
+	return (1);
 	}
 	return (0);
 }
